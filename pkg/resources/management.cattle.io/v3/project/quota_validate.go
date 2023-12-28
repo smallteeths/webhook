fb6b015427c5eebd2c973a3508697eafb05f98b1
smallteeths <503630985@qq.com>
2023-12-28 17:24:51 +0800

    test

--- pkg/resources/management.cattle.io/v3/project/quota_validate.go
+++ pkg/resources/management.cattle.io/v3/project/quota_validate.go
@@ -1,11 +1,21 @@
 package project
 
 import (
+	"github.com/sirupsen/logrus"
+
 	mgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/wrangler/pkg/data/convert"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	quotav1 "k8s.io/apiserver/pkg/quota/v1"
+)
+
+const (
+	// PANDARIA
+	StorageClassPVCQuotaSuffix     = "storageclass.storage.k8s.io/persistentvolumeclaims"
+	StorageClassStorageQuotaSuffix = "storageclass.storage.k8s.io/requests.storage"
+	StorageClassPVCQuotaKey        = "requestsStorageClassPVC"
+	StorageClassStorageQuotaKey    = "requestsStorageClassStorage"
 )
 
 // quotaFits checks whether the quota in the second argument is sufficient for the requested quota in the first argument.
@@ -33,11 +43,40 @@ func convertLimitToResourceList(limit *mgmtv3.ResourceQuotaLimit) (corev1.Resour
 		return nil, err
 	}
 	for key, value := range converted {
-		q, err := resource.ParseQuantity(convert.ToString(value))
-		if err != nil {
-			return nil, err
+		switch value.(type) {
+		case string:
+			q, err := resource.ParseQuantity(convert.ToString(value))
+			if err != nil {
+				return nil, err
+			}
+			toReturn[corev1.ResourceName(key)] = q
+		case map[string]interface{}:
+			valuemaps := value.(map[string]interface{})
+			for k, v := range valuemaps {
+				valueString, ok := v.(string)
+				if ok {
+					q, err := resource.ParseQuantity(valueString)
+					if err != nil {
+						return nil, err
+					}
+
+					var rn corev1.ResourceName
+					if key == StorageClassStorageQuotaKey {
+						resourceNameStr := fmt.Sprintf("%s.%s", k, StorageClassStorageQuotaSuffix)
+						rn = corev1.ResourceName(resourceNameStr)
+					} else if key == validate.StorageClassPVCQuotaKey {
+						resourceNameStr := fmt.Sprintf("%s.%s", k, StorageClassPVCQuotaSuffix)
+						rn = corev1.ResourceName(resourceNameStr)
+					} else {
+						rn = corev1.ResourceName(key)
+					}
+
+					toReturn[rn] = q
+				}
+			}
 		}
-		toReturn[corev1.ResourceName(key)] = q
 	}
+
+	logrus.Infof("toReturn toReturntoReturntoReturn: %+v", toReturn)
 	return toReturn, nil
 }
